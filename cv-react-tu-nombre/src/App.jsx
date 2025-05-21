f55cee7f74c3e083016d7afb5f4131855b478077
elsa patico roto <lalalalla.@gmail.com>
2025-05-21 14:46:34 -0500

    feat: actualizaciones en App.jsx

---
+++ cv-react-tu-nombre/src/App.jsx
@@ -0,0 +1,17 @@
+import CabeceraCV from './components/CabeceraCV';
+import Perfil from './components/Perfil';
+import Experiencia from './components/Experiencia';
+import Educacion from './components/Educacion';
+
+function App() {
+  return (
+    <div>
+      <CabeceraCV />
+      <Perfil />
+      <Experiencia />
+      <Educacion />
+    </div>
+  );
+}
+
+export default App;
