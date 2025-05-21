36cfe683993e6d7fdcfa349b958967094ade0a08
elsa patico roto <lalalalla.@gmail.com>
2025-05-21 14:15:48 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ cv-react-tu-nombre/src/components/CabeceraCV.jsx
@@ -0,0 +1,11 @@
+const CabeceraCV = () => {
+  return (
+    <header>
+      <h1>Simon Vasquez Escobar</h1>
+      <h2>Desarrollador Front-End Jr.</h2>
+      <p>📞 3145241588 | ✉️ simonvasquez550@gmail.com | 📍 Medellin</p>
+    </header>
+  );
+};
+
+export default CabeceraCV;
